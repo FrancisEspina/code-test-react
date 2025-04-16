fc270bb677daaf8f6f0d56db7d2d467ebff46cb2
Francis Espina <faespina@up.edu.ph>
2025-04-16 15:13:10 +0800

    API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import SearchInput from "./components/SearchInput";
 import Itemlist from "./components/Itemlist";
-
+import axios from "axios";
 function App() {
+  let [data, setData] = useState([]);
+  let [isLoading, setLoading] = useState(true);
+
   return (
     <div className="App">
       <div className="container">
@@ -12,7 +15,7 @@ function App() {
             <SearchInput></SearchInput>
           </div>
           <div class="launch__wrapper">
-            <Itemlist></Itemlist>
+            <Itemlist />
           </div>
         </div>
       </div>
--- src/components/Itemlist.jsx
+++ src/components/Itemlist.jsx
@@ -1,58 +1,136 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Spinner from "../components/Spinner/Spinner";
+import InfiniteScroll from "react-infinite-scroll-component";
+// Create some dummy data with IDs or indexes
+import axios from "axios";
 
-const data = Array.from({ length: 50 });
+const LIMIT = 10;
 
-const Itemlist = () => {
+const Itemlist = ({ data, isLoading }) => {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const [launches, setLaunches] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(true);
+
+  const fetchLaunches = async () => {
+    try {
+      const response = await axios.post(
+        "https://api.spacexdata.com/v4/launches/query",
+        {
+          query: {},
+          options: {
+            limit: LIMIT,
+            offset: offset,
+            sort: {
+              date_utc: "asc",
+            },
+          },
+        }
+      );
+
+      const newLaunches = response.data.docs;
+      const total = response.data.totalDocs;
+
+      setLaunches((prev) => [...prev, ...newLaunches]);
+      setOffset((prev) => prev + LIMIT);
+      setHasMore(launches.length + newLaunches.length < total);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching launches:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLaunches();
+  }, []);
+
   return (
     <div className="launch__list">
-      {data &&
-        data.map((entry) => (
-          <>
-            <div className="launch__item ">
-              <div className="launch_body">
-                <div style={{ display: "flex" }}>
-                  <h2>Name</h2>
-                  <div className="launch_status">
-                    <span className="launch__status--success">Status</span>
+      {loading ? (
+        <>
+          <Spinner></Spinner>
+        </>
+      ) : (
+        <>
+          <InfiniteScroll
+            style={{ padding: "5px 5px" }}
+            className="launch__list"
+            next={fetchLaunches}
+            dataLength={launches.length}
+            hasMore={hasMore}
+            loader={<Spinner></Spinner>}
+            endMessage={<p>End of List</p>}
+          >
+            {launches.map((launch, index) => (
+              <div key={index} className="launch__item">
+                <div className="launch_body">
+                  <div style={{ display: "flex" }}>
+                    <h2>
+                      {launch.name} {index + 1}
+                    </h2>
+                    <div className="launch_status">
+                      <span
+                        className={`
+                          ${
+                            launch.success
+                              ? "launch__status--success"
+                              : launch.upcoming
+                              ? "launch__status--info"
+                              : "launch__status--danger"
+                          }
+                        `}
+                      >
+                        {launch.success
+                          ? "Success"
+                          : launch.upcoming
+                          ? "Upcoming"
+                          : "Failed"}
+                      </span>
+                    </div>
+                  </div>
+
+                  <LaunchDetails launch={launch} open={openIndex === index} />
+
+                  <div
+                    onClick={() =>
+                      setOpenIndex(openIndex === index ? null : index)
+                    }
+                    className="btn btn--primary"
+                  >
+                    <div>{openIndex === index ? "Hide" : "View"}</div>
                   </div>
                 </div>
-
-                <div className="details">
-                  <div className="launch__meta">
-                    <div className="launch__meta-item">12 Years Ago </div>
-                    <a href="" className="launch__meta-item">
-                      Article
-                    </a>
-                    <a href="" className="launch__meta-item">
-                      Video
-                    </a>
-                  </div>
-                  <div className="media">
-                    <div>
-                      <img src="https://picsum.photos/200" alt="" />
-                    </div>
-                    <div className="launch__details">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Nobis saepe, suscipit dignissimos commodi consectetur qui
-                      tempora sint porro provident necessitatibus cum ad
-                      similique, tenetur possimus beatae. Ullam itaque nobis
-                      reiciendis.
-                    </div>
-                  </div>
-                </div>
-
-                <div className="btn">View</div>
               </div>
-            </div>
-          </>
-        ))}
-
-      {/* Conditional Rendering */}
-      <Spinner></Spinner>
-      <p>End of List</p>
+            ))}
+          </InfiniteScroll>
+        </>
+      )}
     </div>
   );
+};
+
+const LaunchDetails = ({ launch, open = false }) => {
+  return open ? (
+    <div className="details">
+      <div className="launch__meta">
+        <div className="launch__meta-item">12 Years Ago</div>
+        <a href={launch.links.article} className="launch__meta-item">
+          Article
+        </a>
+        <a href="" className="launch__meta-item">
+          Video
+        </a>
+      </div>
+      <div className="media">
+        <div>
+          <img src="https://picsum.photos/200" alt="" />
+        </div>
+        <div className="launch__details">{launch.details}</div>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default Itemlist;
